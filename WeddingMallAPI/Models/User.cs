c3a12526555f8a420a2f0ece72f984b81b8cc2d7
Shehan Udaraka <shehantharuprocessing@gmail.com>
2023-05-09 12:14:07 +0530

    Modify user class

--- WeddingMallAPI/Models/User.cs
+++ WeddingMallAPI/Models/User.cs
@@ -3,7 +3,7 @@
     public class User
     {
         public int Id { get; set; }
-        public string FName { get; set; } = null;
+        public string FName { get; set; }
         public string LName { get; set; }
         public string Email { get; set; }
         public string UserName { get; set; }
